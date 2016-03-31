b7bcd4c9c8badd50d33d075a9dd5313e33cc61bc
liiu <yliiu_nan-na@docomo.ne.jp>
2016-03-31 19:14:13 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Record - It's new $module
+
+# SYNOPSIS
+
+    use Record;
+
+# DESCRIPTION
+
+Record is ...
+
+# LICENSE
+
+Copyright (C) liiu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+liiu &lt;yliiu\_nan-na@docomo.ne.jp>
---
+++ lib/Record.pm
@@ -0,0 +1,39 @@
+package Record;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Record - It's new $module
+
+=head1 SYNOPSIS
+
+    use Record;
+
+=head1 DESCRIPTION
+
+Record is ...
+
+=head1 LICENSE
+
+Copyright (C) liiu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+liiu E<lt>yliiu_nan-na@docomo.ne.jpE<gt>
+
+=cut
+
